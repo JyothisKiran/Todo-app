e8c9c97ced13c7c11d76f0b14a5377f0bbacd39d
savannah.j <savannah.j@codelynks.com>
2023-08-02 16:59:01 +0530

    working on CRUD of Notes

--- Todo/notes/urls.py
+++ Todo/notes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NoteListView, NoteCreateView
+from .views import NoteListView, NoteCreateView, NoteOperationsView
 
 
 urlpatterns = [
     path('list/',NoteListView.as_view(),name='notes-list'),
     path('create/',NoteCreateView.as_view(),name='notes-create'),
+    path('oper/<int:pk>/',NoteOperationsView.as_view(),name='notes-oper'),
 ]
--- Todo/notes/views.py
+++ Todo/notes/views.py
@@ -4,6 +4,8 @@ from .models import Notes
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 
@@ -18,9 +20,34 @@ class NoteListView(ListAPIView):
 
 class NoteCreateView(CreateAPIView):
     serializer_class = NoteSerializer
+    queryset = Notes.objects.all()
 
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
+        title = serializer.validated_data['title']
+        if self.queryset.filter(title=title).exists():
+            raise ValidationError("Note with same title already exist")
         serializer.save(user=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class NoteOperationsView(RetrieveUpdateDestroyAPIView):
+    serializer_class = NoteSerializer
+
+    def get_object(self,pk):
+        try:
+            note = Notes.objects.get(pk=pk)
+            return note
+        except Notes.DoesNotExist as e:
+            return False
+
+    def get(self, request, pk,*args, **kwargs):
+        obj = self.get_object(pk)
+        print(obj)
+        if obj:
+            serializer = self.serializer_class(instance=obj)
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
